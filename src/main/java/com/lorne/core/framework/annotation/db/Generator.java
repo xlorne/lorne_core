cff741606cdbb4b0041022fd906b8325252c345c
wangliang <1991wangliang@gmail.com>
2017-09-21 14:30:40 +0800

    删除db的注解

--- src/main/java/com/lorne/core/framework/annotation/db/Generator.java
+++
@@ -1,8 +0,0 @@
-package com.lorne.core.framework.annotation.db;
-
-
-@java.lang.annotation.Target(value={java.lang.annotation.ElementType.METHOD})
-@java.lang.annotation.Retention(value=java.lang.annotation.RetentionPolicy.RUNTIME)
-public @interface Generator {
-
-}
